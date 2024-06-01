9292af69a5eac2307b5e1d46dc3271999a766501
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2024-06-01 13:09:45 +0200

    update css

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 const fetchVotingInfo = async (votingSlug: string) => {
   const response = await fetch(
@@ -66,16 +67,16 @@ const List = async () => {
         </div>
       </div>
       <ol className="flex flex-col gap-2">
-        <li className="flex gap-4">
+        <li className="flex gap-4 px-2 items-center justify-between">
           <b>Pos</b>
           <b>Votes</b>
-          <b>Name</b>
-          <b>Username</b>
+          <b>Candidate</b>
+          <b>Vote</b>
         </li>
         {candidates.map((candidate: any, index: number) => (
           <li
             key={candidate.name}
-            className={`flex gap-4 px-2 ${
+            className={`flex gap-4 px-2 items-center justify-between ${
               index === maxWinners - 1 ? "border-white border-b-4" : ""
             }
           ${isUrbe(candidate.username) ? "border-pink-400 border-4" : ""}
@@ -95,10 +96,19 @@ const List = async () => {
                 className="w-[32px] h-[32px] rounded-full object-cover"
               />
               <div className="flex flex-col">
-                <b>{candidate.name}</b>
+                <b>{candidate.name.slice(0, 21)}</b>
                 <span className="text-slate-400">@{candidate.username}</span>
               </div>
             </div>
+            <button className="bg-violet-300 rounded-xl px-5 py-0 h-[40px]">
+              <a
+                className="text-white font-bold my-0 py-0"
+                target="_blank"
+                href={`https://play.talentprotocol.com/votings/eth-cc?open_voting_modal=true&name=${candidate.username}`}
+              >
+                Vote
+              </a>
+            </button>
           </li>
         ))}
       </ol>
