615e38e2fa08ed7819db0642f750b1530e454c56
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2024-06-01 12:53:26 +0200

    update with voting info

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -1,58 +1,104 @@
+import Image from "next/image";
+
 export const dynamic = "force-dynamic";
+
+const fetchVotingInfo = async (votingSlug: string) => {
+  const response = await fetch(
+    `https://play.talentprotocol.com/api/v1/votings/${votingSlug}`,
+    {
+      cache: "no-store",
+    }
+  );
+  const data = await response.json();
+  return data.voting;
+};
+
+const fetchTop100Candidates = async (votingSlug: string) => {
+  const response = await fetch(
+    `https://play.talentprotocol.com/api/v1/votings/${votingSlug}/candidates_leaderboard/?per_page=100`,
+    {
+      cache: "no-store",
+    }
+  );
+  const data = await response.json();
+  return data.leaderboard.results;
+};
 
 const List = async () => {
   // voting slug
   const votingSlug = "eth-cc";
 
-  // this voting has max of 10 winners
-  const maxWinners = 10;
-
-  const response = await fetch(
-    `https://play.talentprotocol.com/api/v1/votings/${votingSlug}/candidates_leaderboard/?per_page=100`
-  );
-  const data = await response.json();
-  const candidates = data.leaderboard.results.sort(
+  const [votingInfo, candidatesUnsorted] = await Promise.all([
+    fetchVotingInfo(votingSlug),
+    fetchTop100Candidates(votingSlug),
+  ]);
+  const candidates = candidatesUnsorted.sort(
     (a: any, b: any) => b.score - a.score
   );
 
+  // this voting has max of 10 winners
+  const maxWinners = votingInfo.winners_count;
+
   const isUrbe = (username: string) => {
-    // talent protocol usernames of urbe team members
+    // talent protocol usernames of urbe team members in this voting
     const urbeUsernames = ["blackiconeth", "soliditydrone"];
     return username && urbeUsernames.includes(username);
   };
 
   return (
-    <ol className="flex flex-col gap-4">
-      <li className="flex gap-4">
-        <b>Pos</b>
-        <b>Votes</b>
-        <b>Name</b>
-      </li>
-      {candidates.map((candidate: any, index: number) => (
-        <li
-          key={candidate.name}
-          className={`flex gap-4 ${
-            index === maxWinners - 1 ? "border-white border-b-4" : ""
-          }
+    <div>
+      <h1 className="text-3xl font-bold">
+        {votingInfo.name} by Talent Protocol
+      </h1>
+      <div className="flex w-full justify-between my-10">
+        <div>
+          <h1 className="text-xl font-bold">Candidates</h1>
+          <p>{candidates.length ?? votingInfo.candidates_count}</p>
+        </div>
+        <div>
+          <h1 className="text-xl font-bold">Prize Pool</h1>
+          <p>{votingInfo.prize_pool} $TALENT</p>
+        </div>
+
+        <div>
+          <h1 className="text-xl font-bold">Votes</h1>
+          <p>{votingInfo.vote_count}</p>
+        </div>
+      </div>
+      <ol className="flex flex-col gap-4">
+        <li className="flex gap-4">
+          <b>Pos</b>
+          <b>Votes</b>
+          <b>Name</b>
+        </li>
+        {candidates.map((candidate: any, index: number) => (
+          <li
+            key={candidate.name}
+            className={`flex gap-4 ${
+              index === maxWinners - 1 ? "border-white border-b-4" : ""
+            }
           ${isUrbe(candidate.username) ? "border-pink-400 border-4" : ""}
           `}
-        >
-          <div className="flex gap-1 items-center">#{index + 1}</div>
-          <div className="flex gap-1 items-center">
-            <b>{candidate.score}</b>
-            Votes
-          </div>
-          <div className="flex gap-1 items-center">
-            <img
-              src={candidate.profile_picture_url ?? ""}
-              alt={candidate.name}
-              className="w-[32px] h-[32px] rounded-full object-cover"
-            />
-            {candidate.name}
-          </div>
-        </li>
-      ))}
-    </ol>
+          >
+            <div className="flex gap-1 items-center">#{index + 1}</div>
+            <div className="flex gap-1 items-center">
+              <b>{candidate.score}</b>
+              Votes
+            </div>
+            <div className="flex gap-1 items-center">
+              <Image
+                src={candidate.profile_picture_url ?? ""}
+                alt={candidate.name}
+                width={32}
+                height={32}
+                className="w-[32px] h-[32px] rounded-full object-cover"
+              />
+              {candidate.name}
+            </div>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 };
 
